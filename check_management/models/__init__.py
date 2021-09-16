fb067100570a6ee1472b71c13961db32a8853293
ayman <aymanshahean@gmail.com>
2021-09-16 15:38:44 +0200

    /

--- check_management/models/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models, account_journal, account_payment, check_history, partial_collection
--- report_account_customer/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import models
-from . import report_general_ledger_customer
